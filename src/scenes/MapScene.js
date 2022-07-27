fed64cc356c319d2abfa86ff747cc956d97b825d
huyldth1908041 <huyldth1908041>
2022-07-27 22:49:25 +0700

    player animation

--- src/scenes/MapScene.js
+++ src/scenes/MapScene.js
@@ -15,10 +15,16 @@ class MapScene extends Phaser.Scene {
   }
 
   preload() {
+    //TODO: config key path in a config file
+    //TODO: separate load assets in a Boot Scene
     this.load.image('terrain-tiles', '/assets/maps/Terrain16x16.png');
     this.load.image('bg-tiles', '/assets/maps/Yellow.png');
     this.load.tilemapTiledJSON('map', '/assets/maps/map1.json');
     this.load.spritesheet('player', '/assets/characters/Virtual Guy/Idle (32x32).png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet('player-run', '/assets/characters/Virtual Guy/Run (32x32).png', {
       frameWidth: 32,
       frameHeight: 32
     });
@@ -38,24 +44,31 @@ class MapScene extends Phaser.Scene {
     this.terrainLayer = this.map.createLayer('ForeGround', this.terrainTiles, 0, 0);
     this.backgroundLayer.setScale(2);
     this.terrainLayer.setScale(2);
-
+    //TODO: Separte to a class
     this.player = this.physics.add.sprite(20, 20, 'player').setScale(1);
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
     this.player.body.setGravityY(300)
     this.anims.create({
-      key: 'idle-left',
+      key: 'idle',
       frames: this.anims.generateFrameNumbers('player', {start: 0, end: 10}),
       frameRate: 11,
       repeat: -1
     });
-    this.player.play('idle-left');
+    this.anims.create({
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('player-run', {start: 0, end: 11}),
+      frameRate: 12,
+      repeat: -1
+    })
+    this.player.play('idle');
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels * 2, this.map.heightInPixels * 2);
     this.physics.world.setBounds(0, 0, this.map.widthInPixels * 2, this.map.heightInPixels * 2);
     this.cameras.main.setZoom(1);
     this.cameras.main.startFollow(this.player);
 
     //collision
+    //TODO: JUST FOR TEST DONT HARD CODE
     this.terrainLayer.setCollision([7, 8, 9, 1, 2, 3, 13, 14, 15, 35, 40, 41, 42])
 
     this.terrainLayer.forEachTile(tile => {
@@ -67,38 +80,29 @@ class MapScene extends Phaser.Scene {
     })
     this.physics.add.collider(this.player, this.terrainLayer);
     this.cursors = this.input.keyboard.createCursorKeys();
-    // this.controlConfig = {
-    //   camera: this.cameras.main,
-    //   left: this.cursors.left,
-    //   right: this.cursors.right,
-    //   up: this.cursors.up,
-    //   down: this.cursors.down,
-    //   speed: 0.5
-    // };
-    // this.controls = new Phaser.Cameras.Controls.FixedKeyControl(this.controlConfig);
   }
 
 
   update(time, delta) {
-    // console.log(this.player.body.touching.down)
     // console.log(this.player.body.velocity.y)
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
-
-      // this.player.anims.play('left', true);
+      this.player.flipX = true
+      this.player.anims.play('run', true);
     } else if (this.cursors.right.isDown) {
+      this.player.anims.play('run', true);
       this.player.setVelocityX(160);
+      this.player.flipX = false
 
       // this.player.anims.play('right', true);
     } else {
       this.player.setVelocityX(0);
-
+      this.player.anims.play('idle', true);
       // this.player.anims.play('turn');
     }
 
-    if (this.cursors.up.isDown) {
-      console.log('here')
-      this.player.setVelocityY(-330);
+    if (this.cursors.up.isDown && this.player.body.blocked.down) {
+      this.player.setVelocityY(-350);
     }
   }
 }
